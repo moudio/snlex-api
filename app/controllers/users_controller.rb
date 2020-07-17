df9316a20caaa4107b6b889853850ab058cbdca6
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-17 13:31:51 +0000

    add the create method to User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: {
+        status: 'created',
+        user: @user
+      }
+    end
+  end
+
+
+
+  private
+  def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
