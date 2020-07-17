d9daaa8799031db1942b4d24a26571dc920e7767
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-17 14:14:10 +0000

    Test the user request for username length

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
 
+  it "returns status created if user is created" do
+      post '/api/users', params: {user: { username: 'John', email: 'john@gmail.com', password: 'password', password_confirmation: 'password'}}
+      expect(JSON(response.body)['status']).to eq("created")
+  end
+
+  it "return error messages if user is not valid" do
+    post '/api/users', params: {user: {username: 'Mo', email: 'mo@gmail.com', password: 'password', password_confirmation: 'password'}}
+    expect(JSON(response.body)['status']).to eq(500)
+    expect(JSON(response.body)['errors'].first).to match('Username is too short')
+  end
+
 end
