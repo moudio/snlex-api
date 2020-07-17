8bee8740d932fe3d3f79df11feb9ab3c9772757c
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-17 13:23:23 +0000

    Create the Sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   has_secure_password
-  validates :username, presence: true, uniqueness: {case_sensitive: false},  length: {in: 4..15}
+  validates :username, presence: true, uniqueness: {case_sensitive: false},  length: {in: 4..10}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
-
 end
