297017f348d3496b84d5073c19d0e8d89dff516c
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-17 10:58:30 +0000

    Add first name and last name to faker

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :username, presence: true
   validates :email, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
 end
