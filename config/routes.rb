e082c5417afcacbe43e802282cdd960d015c42cb
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-17 13:54:13 +0000

    Add scoping to the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-  post 'login' => 'sessions#create'
-
+    scope :api do
+      resources :users
+      post 'login' => 'sessions#create'
+    end
 end
