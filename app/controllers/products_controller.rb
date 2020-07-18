f5e4b3bfdbed89cdaea7da4538353b4422d3589c
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-18 16:48:36 +0000

    Test the index action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,16 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products =  Product.all
+    if(@products)
+    render json: {
+      products: @products
+    }
+  else
+    render json: {
+      message: "You don't have the rights for this request"
+    }
+
+  end
+end 
 end
--- spec/requests/products_request_spec.rb
+++ spec/requests/products_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
-
+  let!(:products) {create_list(:product, 20)}
+  context "Testing Products CRUD" do
+    it "tests if the user can list all products" do
+      get '/api/products/'
+      expect(JSON(response.body)['products'].length).to eq(20)
+    end
+  end
 end
